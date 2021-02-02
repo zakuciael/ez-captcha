ed8e19e8b79c52123a92fa43a55f722a3dda4c37
Krzysztof Saczuk <zakuciael@outlook.com>
2021-02-02 11:28:18 +0100

    sendAnswer() method can now throw InvalidResponse error.

--- src/lib/sendAnswer.ts
+++ src/lib/sendAnswer.ts
@@ -1,5 +1,6 @@
 import { CAPTCHA_BASE_URL, CAPTCHA_LOCALE, CAPTCHA_USER_AGENT, CaptchaConfig } from "./globals";
 import fetch from "node-fetch";
+import { InvalidResponseError } from "./InvalidResponseError";
 
 export const sendAnswer = async (challengeId: string, answer: number): Promise<CaptchaConfig> => {
     return fetch(`${CAPTCHA_BASE_URL}/${challengeId}/${CAPTCHA_LOCALE}`, {
@@ -10,5 +11,8 @@ export const sendAnswer = async (challengeId: string, answer: number): Promise<C
             "User-Agent": CAPTCHA_USER_AGENT,
         },
         body: JSON.stringify({ answer: answer }),
-    }).then((res) => res.json() as Promise<CaptchaConfig>);
+    }).then((res) => {
+        if (res.ok) return res.json();
+        else throw new InvalidResponseError(res.status, res.statusText);
+    }) as Promise<CaptchaConfig>;
 };
